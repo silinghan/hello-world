6cb01136879bba901633579ea85af4307551e7a6
Eric Han <eric.han@health.nsw.gov.au>
2023-09-18 15:38:40 +1000

    just a new ng

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  env: 'prod',
+  production: true,
+  mail_url: "",
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  },
+  domain: "https://helloworld-d8d88.firebaseapp.com/",
+};

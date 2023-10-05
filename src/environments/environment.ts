825e693fe82ed7adc68bbf76db2743a22ace860d
Eric Han <eric.han@health.nsw.gov.au>
2023-10-06 09:42:23 +1100

    test

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import {firebase} from './local.environment'
-
-export const environment = {
-  env: 'local',
-  production: 'false',
-  mail_url: 'http://localhost:5001/hbclub-919aa/us-central1',
-  domain: "https://hbc666.club",
-  ...firebase,
-};
